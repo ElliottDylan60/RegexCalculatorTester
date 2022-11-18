2f666de762a4a16e1e0263fda762cfd3961fb01a
ElliottDylan60 <elliottdylan60@gmail.com>
2022-11-17 22:08:07 -0800

    Now Generates on sperate thread and updates progress bar

--- ExpressionMaker/Form1.Designer.cs
+++ ExpressionMaker/Form1.Designer.cs
@@ -38,7 +38,14 @@
             this.lbIterations = new System.Windows.Forms.Label();
             this.lbInfo = new System.Windows.Forms.Label();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.numEquations = new System.Windows.Forms.NumericUpDown();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Generator = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.numIterations)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numEquations)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -49,7 +56,7 @@
             this.richTextBox1.ForeColor = System.Drawing.Color.LightGray;
             this.richTextBox1.Location = new System.Drawing.Point(12, 180);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(776, 258);
+            this.richTextBox1.Size = new System.Drawing.Size(776, 244);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
@@ -100,6 +107,11 @@
             this.numIterations.Name = "numIterations";
             this.numIterations.Size = new System.Drawing.Size(176, 22);
             this.numIterations.TabIndex = 46;
+            this.numIterations.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
             // 
             // panel13
             // 
@@ -132,12 +144,82 @@
             this.lbInfo.Text = "ⓘ";
             this.toolTip1.SetToolTip(this.lbInfo, "Complexity of generated equation");
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(194, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 42);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "ⓘ";
+            this.toolTip1.SetToolTip(this.label1, "Total number of equations to generate");
+            // 
+            // numEquations
+            // 
+            this.numEquations.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.numEquations.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numEquations.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.numEquations.ForeColor = System.Drawing.Color.LightGray;
+            this.numEquations.Location = new System.Drawing.Point(12, 120);
+            this.numEquations.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numEquations.Name = "numEquations";
+            this.numEquations.Size = new System.Drawing.Size(176, 22);
+            this.numEquations.TabIndex = 50;
+            this.numEquations.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Location = new System.Drawing.Point(12, 144);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(176, 3);
+            this.panel2.TabIndex = 49;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightGray;
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 25);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "Total Equations";
+            // 
+            // Generator
+            // 
+            this.Generator.WorkerReportsProgress = true;
+            this.Generator.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Generator_DoWork);
+            this.Generator.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.Generator_ProgressChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 430);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 10);
+            this.progressBar1.TabIndex = 53;
+            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numEquations);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.lbInfo);
             this.Controls.Add(this.lbIterations);
             this.Controls.Add(this.numIterations);
@@ -151,6 +233,7 @@
             this.Text = "Calculator Tester";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numIterations)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numEquations)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,6 +250,12 @@
         private System.Windows.Forms.Label lbIterations;
         private System.Windows.Forms.Label lbInfo;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numEquations;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker Generator;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- ExpressionMaker/Form1.cs
+++ ExpressionMaker/Form1.cs
@@ -13,11 +13,24 @@ namespace ExpressionMaker
 {
     public partial class Dashboard : Form
     {
+        /// <summary>
+        /// All possible valid syntax
+        /// </summary>
         List<string> validSyntax = new List<string>();
+        /// <summary>
+        /// All possible invalid syntax
+        /// </summary>
+        List<string> invalidSyntax = new List<string>();
+        /// <summary>
+        /// Initialize Components
+        /// </summary>
         public Dashboard()
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// Initialize valid syntax list
+        /// </summary>
         private void InitializeValidSyntax() {
             validSyntax.Add("({i})");
             validSyntax.Add("-{i}");
@@ -33,49 +46,82 @@ namespace ExpressionMaker
             validSyntax.Add("log({i})");
             validSyntax.Add("ln({i})");
         }
+        /// <summary>
+        /// Initialize invalid syntax list
+        /// </summary>
+        private void InitilizeInvalidSyntax() { 
+        
+        }
+        /// <summary>
+        /// When Form loads
+        /// </summary>
         private void Form1_Load(object sender, EventArgs e)
         {
             InitializeValidSyntax();
-            string expression = "{i}";
-            Console.WriteLine(expression);
-            // for each variable in the expressin replacee it with a validSyntax expression
-            for (int j = 0; j < 5; j++)
-            {
-                int i = 0;
-                expression = Regex.Replace(expression, "{i}", m => string.Format("{{0}}", i++), RegexOptions.IgnoreCase); // replace all i with number
-                string[] expressionsToAdd = new string[i];
-                while (i > 0)
-                {
-                    i--;
-                    Random rand = new Random(Guid.NewGuid().GetHashCode());
-                    expressionsToAdd[i] = validSyntax[rand.Next(validSyntax.Count())];
-
-                }
-
-                expression = String.Format(expression, expressionsToAdd);
-                Console.WriteLine(expression);
-                expression = Regex.Replace(expression, @"[0-9]+", m => "i", RegexOptions.IgnoreCase);
-            }
-
-
-
-
-            /*
-            var s = "({100}+cos({1}))";
-            var i = 0;
-            //var result = Regex.Replace(s, "i", m => string.Format("{0}", i++), RegexOptions.IgnoreCase); // replace in s, value "i", with m where m = i++
-            var result = Regex.Replace(s, @"[0-9]+", m => "i", RegexOptions.IgnoreCase); // replaces any number in string s with an i
-            Console.WriteLine(result);
-
-            
-            string[] test = new string[] { "test", "test", "test"};
-            Console.WriteLine("First {0} Second {1} Third {2}", test);
-            */
         }
-
+        /// <summary>
+        /// Generate any number of equations
+        /// </summary>
         private void btnGenerate_Click(object sender, EventArgs e)
         {
+            richTextBox1.Clear();
+            lbEmpty.Visible = false;
+            Generator.RunWorkerAsync();
+            
+        }
+        /// <summary>
+        /// Writes to rich text box
+        /// </summary>
+        /// <param name="Message">Print first part as normal color</param>
+        /// <param name="redMessage">Print second part as specified color</param>
+        /// <param name="color">Color of second part</param>
+        private void writeLog(string Message, string colorMessage, Color color) {
+            /// Normal
+            richTextBox1.AppendText(Message);
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
 
+            /// Red
+            richTextBox1.SelectionColor = color;
+            richTextBox1.AppendText(colorMessage);
+
+            /// Normal
+            richTextBox1.SelectionColor = richTextBox1.ForeColor;
+        }
+
+        private void Generator_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int a = 0; a < (int)numEquations.Value; a++)
+            {
+                string expression = "{i}";
+                // for each variable in the expressin replacee it with a validSyntax expression
+                for (int j = 0; j < (int)numIterations.Value; j++)
+                {
+                    int i = 0;
+                    expression = Regex.Replace(expression, "{i}", m => string.Format("{{0}}", i++), RegexOptions.IgnoreCase); // replace all i with number
+                    string[] expressionsToAdd = new string[i];
+                    while (i > 0)
+                    {
+                        i--;
+                        Random rand = new Random(Guid.NewGuid().GetHashCode());
+                        expressionsToAdd[i] = validSyntax[rand.Next(validSyntax.Count())];
+                    }
+                    expression = String.Format(expression, expressionsToAdd);
+                    expression = Regex.Replace(expression, @"[0-9]+", m => "i", RegexOptions.IgnoreCase);
+                }
+                // Report current generation percent
+                Generator.ReportProgress((int)((a / numEquations.Value) * 100), "Working...");
+                // Delegate to allow changes on sperate UI
+                this.Invoke((MethodInvoker)delegate{
+                    writeLog(expression + " ---------------- ", "PASSED\n", Color.Green);
+                });
+            }
+            Generator.ReportProgress(100, "Complete!");
+        }
+
+        private void Generator_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //Console.WriteLine(e.ProgressPercentage);
+            progressBar1.Value = e.ProgressPercentage;
         }
     }
 }
